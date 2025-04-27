c41755091bcfb3fa399bfd54f927df295f3eb953
Mvrlex <marl3x11@hotmail.com>
2025-04-27 14:50:10 +0200

    feat: add firmware
    Took 2 hours 41 minutes

---
+++ firmware/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x3A, 0x7F, 0xB2, 0x1C, 0x8E, 0x55, 0xD9, 0x04}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 11 }
